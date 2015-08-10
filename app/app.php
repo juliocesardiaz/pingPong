6cc2c5dba6890c0186d78e661690462a24ef5f85
Julio Diaz & Adam Won <student@epicodus.com>
2015-08-10 14:51:02 -0700

    Wrote some Twig files

---
+++ app/app.php
@@ -0,0 +1,20 @@
+<?php 
+	require_once __DIR__."/../vendor/autoload.php";
+	require_once __DIR__."/../src/PingPong.php";
+	
+	$app = new Silex\Application ();
+	
+	$app->register(new Silex\Provider\TwigServiceProvider(), array(
+		'twig.path' => __DIR__.'/../views'
+	));
+	
+	$app->get("/", function() use ($app){
+		return $app ['twig']->render('form.html.twig');
+	});
+	
+	$app->get("/view_ping_pong", function() use ($app)) {
+		$myPingPong = new PingPong;
+		$pingPong = $myPingPong->makePingPong($_GET['number']);
+		return $app['twig']->render('pingPong.html.twig', array('result' => $pingPong));
+	});
+?>
